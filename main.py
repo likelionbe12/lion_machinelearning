6be2cf472719ddb03ac5a64274ce3e097a42d6cb
ImJonghan <likelionbe12@gmail.com>
2024-10-29 11:21:08 +0900

    셀렉트 박스 추가

--- main.py
+++ main.py
@@ -4,3 +4,10 @@ import streamlit as st
 
 st.title("MachineLearning")
 st.header("Stock 예측 프로그램")
+
+option = st.selectbox(
+    "관심 기업은?",
+    ("테슬라", "OpenAI", "Meta"),
+)
+
+st.write("You selected:", option)
