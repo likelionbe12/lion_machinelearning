c878964f79b3849e465d9c6ac61398e8ed2d0289
ImJonghan <likelionbe12@gmail.com>
2024-10-29 15:24:14 +0900

    페이지 추가

--- main.py
+++ main.py
@@ -1,32 +1,12 @@
 # streamlit에 쓸 파일
 # ml은 colab
 import streamlit as st
-from sklearn.ensemble import RandomForestRegressor
-import numpy as np
-import pickle
 
-st.title("MachineLearning")
-st.header("캘리포니아 집값 예측 프로그램")
-cols = ['MedInc','HouseAge','AveRooms','AveBedrms', 'Population','AveOccup','Latitude','Longitude']
-datas = [0]*8
-for idx, col in enumerate(cols):
-    datas[idx] = st.number_input(col)
-# MedInc= st.number_input("MedInc")
-# HouseAge= st.number_input("HouseAge")
-# AveRooms= st.number_input("AveRooms")
-# AveBedrms= st.number_input("AveBedrms")
-# Population= st.number_input("Population")
-# AveOccup= st.number_input("AveOccup")
-# Latitude= st.number_input("Latitude")
-# Longitude= st.number_input("Longitude")
+def page2():
+    st.title("Second page")
 
-
-x = np.array(datas).reshape(1, -1)
-# x.shape
-
-with open("rf_house.pickle", 'rb') as f:
-    rf_model = pickle.load(f)
-
-y = rf_model.predict(x)
-
-st.header("예측되는 집값은 {}입니다.".format(y))
+pg = st.navigation([
+    st.Page("page1.py", title="캘리포니아 집값 예측", icon="🔥"),
+    st.Page(page2, title="Second page", icon=":material/favorite:"),
+])
+pg.run()
