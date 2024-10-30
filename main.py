9f22690f6bf6548f08b517afb4c24ba1cb48c64b
ImJonghan <likelionbe12@gmail.com>
2024-10-30 10:42:18 +0900

    스톡 페이지 추가

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 # streamlit에 쓸 파일
 # ml은 colab
 import streamlit as st
+from pages import page2
 
-def page2():
-    st.title("Second page")
 
 pg = st.navigation([
+    st.Page(page2, title="Stock 데이터분석", icon=":material/favorite:"),
     st.Page("page1.py", title="캘리포니아 집값 예측", icon="🔥"),
-    st.Page(page2, title="Second page", icon=":material/favorite:"),
 ])
 pg.run()
